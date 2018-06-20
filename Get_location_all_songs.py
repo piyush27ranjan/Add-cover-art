7a867d41e60339d9c434528082ad3fb90bdd1885
piyush27ranjan <piyush.27ranjan@gmail.com>
2018-06-20 12:21:54 +0530

    update scrape google images

--- Get_location_all_songs.py
+++ Get_location_all_songs.py
@@ -6,3 +6,41 @@ for root, dirs, files in os.walk(r'D:\\'):
     for file in files:
         if file.endswith('.mp3'):
             asps.append(os.path.join(root, file))
+     
+       #Get songs name with location 
+music_names = []
+for i in range(len(asps)):
+    music_names.append([])
+    music_names[i].append(asps[i].split("\\")[-1])
+    music_names[i].append(asps[i])
+    
+
+      #Strip track no and numbers from the song names
+#Get list of numbers
+num = []
+for i in range(10):
+    num.append(str(i))
+num.append("-")
+for i in range(len(music_names)):
+    for a in music_names[i][0]:
+        if a in num :
+            music_names[i][0] = music_names[i][0][1:]
+        else:
+            break
+        
+    #Remove extension from song names
+for i in range(len(music_names)):
+    music_names[i][0] = "".join(music_names[i][0].split('.')[:-1])     
+
+    #replace '-','_','320','Kbps','kbps' sign with ' '
+for i in range(len(music_names)):
+    music_names[i][0] = music_names[i][0].replace("-"," ")
+    music_names[i][0] = music_names[i][0].replace("_"," ")
+    music_names[i][0] = music_names[i][0].replace("320"," ")
+    music_names[i][0] = music_names[i][0].replace("Kbps"," ")
+    music_names[i][0] = music_names[i][0].replace("kbps"," ")
+        
+    #remove anything in between (),[],{}
+import re 
+for i in range(len(music_names)):
+    music_names[i][0] = re.sub("[\(\[].*?[\)\]]", "", music_names[i][0])
--- scrape_image_from_google_images.py
+++ scrape_image_from_google_images.py
@@ -6,26 +6,29 @@ def get_soup(url,header):
     return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
 query = input("Enter the keyword: ")
-query= query.split()
-query='+'.join(query)
-url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
-header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-soup = get_soup(url,header)
 
-Images_Links = []
 
-for a in soup.find_all("div",{"class":"rg_meta"}):
-	    link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-	    Images_Links.append((link,Type))
-        
-ActualImages = []
-
-for i in range(len(Images_Links)):
-    if Images_Links[i][0].split(".")[-1] in ["png","jpeg","jpg"]:
-        ActualImages.append(Images_Links[i][0])
-        
-for i in range(10):
-    try:
-        urllib.request.urlretrieve(ActualImages[i],str(i)+"."+ActualImages[i].split(".")[-1])
-    except:
-        continue
+def scrape_google_image(query,num):
+    query= query.split()
+    query='+'.join(query)
+    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    soup = get_soup(url,header)
+    
+    Images_Links = []
+    
+    for a in soup.find_all("div",{"class":"rg_meta"}):
+    	    link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
+    	    Images_Links.append((link,Type))
+            
+    ActualImages = []
+    
+    for i in range(len(Images_Links)):
+        if Images_Links[i][0].split(".")[-1] in ["png","jpeg","jpg"]:
+            ActualImages.append(Images_Links[i][0])
+            
+    for i in range(len(Images_Links)):
+        try:
+            urllib.request.urlretrieve(ActualImages[i],str(i)+"."+ActualImages[i].split(".")[-1])
+        except:
+            continue
