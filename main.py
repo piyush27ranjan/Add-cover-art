939069c1c109ff1380f0d3ba2ea1989b40154991
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-12-06 03:46:44 +0530

    Added Cli interface

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import eyed3
 import scrape_image_from_google_images
 from PIL import ImageTk, Image
 import tkinter as tk
+import argparse
+
 
 
 def tkinter_window(location, audiofile):
@@ -39,61 +41,62 @@ def add_image(location, audiofile):
     audiofile.tag.save()
 
 
-print("Finding all .mp3 files")
-asps = []
-
-if os.name == 'nt':
-    dir = r""'D:\\'""
-elif os.name == 'posix':
-    dir = './'  # current dir of project
-for root, dirs, files in os.walk(dir):
-    for file in files:
-        if file.endswith('.mp3'):
-            asps.append(os.path.join(root, file))
-
-print("All files found")
-# Get songs name with location
-music_names = []
-for i in range(len(asps)):
-    music_names.append([])
-    music_names[i].append(asps[i].split("\\")[-1])
-    music_names[i].append(asps[i])
+if __name__ == '__main':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', nargs='?', default=os.getcwd())
+    args=parser.parse_args()
+    
+    print("Finding all .mp3 files in ",args.directory)
+    asps = []        
+    for root, dirs, files in os.walk(args.directory):
+        for file in files:
+            if file.endswith('.mp3'):
+                asps.append(os.path.join(root, file))
+    print(len(asps),' Files Found, Processing...')
+    
+    # Get songs name with location
+    music_names = []
+    for i in range(len(asps)):
+        music_names.append([])
+        music_names[i].append(asps[i].split("\\")[-1])
+        music_names[i].append(asps[i])
 
     # Strip track no and numbers from the song names
-# Get list of numbers
-num = []
-for i in range(10):
-    num.append(str(i))
-num.append("-")
-for i in range(len(music_names)):
-    for a in music_names[i][0]:
-        if a in num:
-            music_names[i][0] = music_names[i][0][1:]
-        else:
-            break
+    # Get list of numbers
+    num = []
+    for i in range(10):
+        num.append(str(i))
+    num.append("-")
+    for i in range(len(music_names)):
+        for a in music_names[i][0]:
+            if a in num:
+                music_names[i][0] = music_names[i][0][1:]
+            else:
+                break
 
-    # Remove extension from song names
-for i in range(len(music_names)):
-    music_names[i][0] = "".join(music_names[i][0].split('.')[:-1])
+        # Remove extension from song names
+    for i in range(len(music_names)):
+        music_names[i][0] = "".join(music_names[i][0].split('.')[:-1])
 
-    # replace '-','_','320','Kbps','kbps' sign with ' '
-for i in range(len(music_names)):
-    music_names[i][0] = music_names[i][0].replace("-", " ")
-    music_names[i][0] = music_names[i][0].replace("_", " ")
-    music_names[i][0] = music_names[i][0].replace("320", " ")
-    music_names[i][0] = music_names[i][0].replace("Kbps", " ")
-    music_names[i][0] = music_names[i][0].replace("kbps", " ")
+        # replace '-','_','320','Kbps','kbps' sign with ' '
+    for i in range(len(music_names)):
+        music_names[i][0] = music_names[i][0].replace("-", " ")
+        music_names[i][0] = music_names[i][0].replace("_", " ")
+        music_names[i][0] = music_names[i][0].replace("320", " ")
+        music_names[i][0] = music_names[i][0].replace("Kbps", " ")
+        music_names[i][0] = music_names[i][0].replace("kbps", " ")
 
-    # remove anything in between (),[],{}
-for i in range(len(music_names)):
-    music_names[i][0] = re.sub(r"[\(\[].*?[\)\]]", "", music_names[i][0])
+        # remove anything in between (),[],{}
+    for i in range(len(music_names)):
+        music_names[i][0] = re.sub(r"[\(\[].*?[\)\]]", "", music_names[i][0])
 
-for i in range(len(music_names)):
-    audiofile = eyed3.load(music_names[i][1])
-    song_file_name = scrape_image_from_google_images.scrape_google_image(music_names[i][0] +
-                                                                         " song cover art", name=music_names[i][0])
-    try:
-        Image.open(song_file_name)
-    except BaseException:
-        continue
-    tkinter_window(song_file_name, audiofile)
+    for i in range(len(music_names)):
+        audiofile = eyed3.load(music_names[i][1])
+        song_file_name = scrape_image_from_google_images.scrape_google_image(music_names[i][0] +
+                                                                             " song cover art", name=music_names[i][0])
+        try:
+            Image.open(song_file_name)
+        except BaseException:
+            continue
+        tkinter_window(song_file_name, audiofile)
