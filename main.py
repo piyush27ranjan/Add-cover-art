8fbc91789cec07793213aada8d630c27d3ce30e1
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-12-06 01:59:54 +0530

    Formatting

--- main.py
+++ main.py
@@ -1,97 +1,99 @@
+import re
 import os
 import eyed3
 import scrape_image_from_google_images
 from PIL import ImageTk, Image
 import tkinter as tk
 
-def tkinter_window(location,audiofile):
+
+def tkinter_window(location, audiofile):
     window = tk.Tk()
     window.title("Add cover art")
     window.geometry("300x250")
-    name = tk.Label(window,text='Do u want this images as cover art' )
+    name = tk.Label(window, text='Do u want this images as cover art')
     path = location
-    img = ImageTk.PhotoImage(Image.open(path).resize((150,150), Image.ANTIALIAS))
-    panel = tk.Label(window, image = img)
-    song_query_question = tk.Label(window,text='Enter text to search' )
+    img = ImageTk.PhotoImage(Image.open(path).resize((150, 150), Image.ANTIALIAS))
+    panel = tk.Label(window, image=img)
+    song_query_question = tk.Label(window, text='Enter text to search')
     song_query = tk.Entry(window)
-    OK= tk.Button(window, text="Okay",command = add_image(location,audiofile))
-    Cancel = tk.Button(window, text="Cancel",command = window.destroy)
-    Next= tk.Button(window, text="Next",command = window.destroy)
+    OK = tk.Button(window, text="Okay", command=add_image(location, audiofile))
+    Cancel = tk.Button(window, text="Cancel", command=window.destroy)
+    Next = tk.Button(window, text="Next", command=window.destroy)
 
-    name.grid(column=0,row=0,columnspan=6)
-    panel.grid(column=0,row=1,columnspan=3,rowspan =3)
-    OK.grid(column=0,row=4,columnspan=2)
-    Next.grid(column=2,row=4)
-    song_query_question.grid(column=3,row=1,columnspan=3)
-    song_query.grid(column=3,row=2,columnspan=3)
-    Cancel.grid(column=4,row=4,columnspan=2)
+    name.grid(column=0, row=0, columnspan=6)
+    panel.grid(column=0, row=1, columnspan=3, rowspan=3)
+    OK.grid(column=0, row=4, columnspan=2)
+    Next.grid(column=2, row=4)
+    song_query_question.grid(column=3, row=1, columnspan=3)
+    song_query.grid(column=3, row=2, columnspan=3)
+    Cancel.grid(column=4, row=4, columnspan=2)
 
     window.mainloop()
 
-def add_image(location,audiofile):
-    if (audiofile.tag == None):
+
+def add_image(location, audiofile):
+    if (audiofile.tag is None):
         audiofile.initTag()
     print(audiofile.tag.album_artist)
-    audiofile.tag.images.set(3, open(location,'rb').read(), 'image/jpeg')
+    audiofile.tag.images.set(3, open(location, 'rb').read(), 'image/jpeg')
     audiofile.tag.save()
+
 
 print("Finding all .mp3 files")
 asps = []
 
 if os.name == 'nt':
-	dir=r""'D:\\'""
+    dir = r""'D:\\'""
 elif os.name == 'posix':
-    dir='./'    #current dir of project
+    dir = './'  # current dir of project
 for root, dirs, files in os.walk(dir):
     for file in files:
         if file.endswith('.mp3'):
             asps.append(os.path.join(root, file))
 
 print("All files found")
-       #Get songs name with location
+# Get songs name with location
 music_names = []
 for i in range(len(asps)):
     music_names.append([])
     music_names[i].append(asps[i].split("\\")[-1])
     music_names[i].append(asps[i])
 
-
-      #Strip track no and numbers from the song names
-#Get list of numbers
+    # Strip track no and numbers from the song names
+# Get list of numbers
 num = []
 for i in range(10):
     num.append(str(i))
 num.append("-")
 for i in range(len(music_names)):
     for a in music_names[i][0]:
-        if a in num :
+        if a in num:
             music_names[i][0] = music_names[i][0][1:]
         else:
             break
 
-    #Remove extension from song names
+    # Remove extension from song names
 for i in range(len(music_names)):
     music_names[i][0] = "".join(music_names[i][0].split('.')[:-1])
 
-    #replace '-','_','320','Kbps','kbps' sign with ' '
+    # replace '-','_','320','Kbps','kbps' sign with ' '
 for i in range(len(music_names)):
-    music_names[i][0] = music_names[i][0].replace("-"," ")
-    music_names[i][0] = music_names[i][0].replace("_"," ")
-    music_names[i][0] = music_names[i][0].replace("320"," ")
-    music_names[i][0] = music_names[i][0].replace("Kbps"," ")
-    music_names[i][0] = music_names[i][0].replace("kbps"," ")
+    music_names[i][0] = music_names[i][0].replace("-", " ")
+    music_names[i][0] = music_names[i][0].replace("_", " ")
+    music_names[i][0] = music_names[i][0].replace("320", " ")
+    music_names[i][0] = music_names[i][0].replace("Kbps", " ")
+    music_names[i][0] = music_names[i][0].replace("kbps", " ")
 
-    #remove anything in between (),[],{}
-import re
+    # remove anything in between (),[],{}
 for i in range(len(music_names)):
-    music_names[i][0] = re.sub("[\(\[].*?[\)\]]", "", music_names[i][0])
+    music_names[i][0] = re.sub(r"[\(\[].*?[\)\]]", "", music_names[i][0])
 
 for i in range(len(music_names)):
     audiofile = eyed3.load(music_names[i][1])
-    song_file_name=scrape_image_from_google_images.scrape_google_image(music_names[i][0]+    \
-                                                        " song cover art",name=music_names[i][0])
+    song_file_name = scrape_image_from_google_images.scrape_google_image(music_names[i][0] +
+                                                                         " song cover art", name=music_names[i][0])
     try:
         Image.open(song_file_name)
-    except:
+    except BaseException:
         continue
-    tkinter_window(song_file_name,audiofile)
+    tkinter_window(song_file_name, audiofile)
--- scrape_image_from_google_images.py
+++ scrape_image_from_google_images.py
@@ -3,54 +3,54 @@ import urllib
 import json
 import os
 
-def get_soup(url,header):
-    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
+
+def get_soup(url, header):
+    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url, headers=header)), 'html.parser')
 
 
-
-def scrape_google_image(query,num = 1,address = None, name = None):
+def scrape_google_image(query, num=1, address=None, name=None):
     if not os.path.exists("./images"):
         os.makedirs("./images")
-    query= query.split()
-    query='+'.join(query)
-    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
-    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-    soup = get_soup(url,header)
-    
+    query = query.split()
+    query = '+'.join(query)
+    url = "https://www.google.co.in/search?q=" + query + "&source=lnms&tbm=isch"
+    header = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    soup = get_soup(url, header)
+
     Images_Links = []
-    i=0
-    print("Finding images of "+query.replace("+"," "))
-    for a in soup.find_all("div",{"class":"rg_meta"}):
-        link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-        Images_Links.append((link,Type))
-         
-            
+    i = 0
+    print("Finding images of " + query.replace("+", " "))
+    for a in soup.find_all("div", {"class": "rg_meta"}):
+        link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
+        Images_Links.append((link, Type))
+
     ActualImages = []
     print("checking valid url")
     for i in range(len(Images_Links)):
-        if Images_Links[i][0].split(".")[-1] in ["png","jpeg","jpg"]:
+        if Images_Links[i][0].split(".")[-1] in ["png", "jpeg", "jpg"]:
             ActualImages.append(Images_Links[i][0])
-    if name == None:
+    if name is None:
         name = query
     print("\n")
     for i in range(num):
         try:
             if num > 0:
                 if name is None:
-                    urllib.request.urlretrieve(ActualImages[i],str(i)+"."+ActualImages[i].split(".")[-1])
-                    print("Images Downloaded: "+ str((i+1)/num*100) + "%\n")
+                    urllib.request.urlretrieve(ActualImages[i], str(i) + "." + ActualImages[i].split(".")[-1])
+                    print("Images Downloaded: " + str((i + 1) / num * 100) + "%\n")
                 else:
-                    urllib.request.urlretrieve(ActualImages[i],"./images/"+name+"."+ActualImages[i].split(".")[-1])
-                    print("Images Downloaded: "+ str((i+1)/num*100) + "%\n")
+                    urllib.request.urlretrieve(ActualImages[i], "./images/" + name + "." + ActualImages[i].split(".")[-1])
+                    print("Images Downloaded: " + str((i + 1) / num * 100) + "%\n")
             else:
                 break
-        except:
+        except BaseException:
             continue
-    return "./images/"+name+"."+ActualImages[0].split(".")[-1]
-        
+    return "./images/" + name + "." + ActualImages[0].split(".")[-1]
+
+
 if __name__ == '__main__':
     query = input("Enter the keyword: ")
     no = int(input("Enter no of images (max = 100): "))
     address = input("Enter the directory")
     name = input("Name of the file (for one image): ")
-    scrape_google_image(query,no,address,name)
+    scrape_google_image(query, no, address, name)
