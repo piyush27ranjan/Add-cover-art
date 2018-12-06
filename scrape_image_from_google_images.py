d9bc179d85d672a2e715a784e1c8cdf51aed9bdb
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-12-06 19:41:27 +0530

    Refactored to scarper file Now scrape_google_image saves all the images of a given query in one file Moreover you can give it a max_num argument to restrict number of images downloaded Address parameter was removed

--- scrape_image_from_google_images.py
+++ scrape_image_from_google_images.py
@@ -1,56 +1,45 @@
-from bs4 import BeautifulSoup
-import urllib.request
+import itertools
+
 import json
 import os
+import urllib.error
+import urllib.request
+from bs4 import BeautifulSoup
 
 
-def get_soup(url, header):
+def get_soup(url):
+    header = {'User-Agent':
+                  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
     return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url, headers=header)), 'html.parser')
 
 
-def scrape_google_image(query, num=1, address=None, name=None):
-    if not os.path.exists("./images"):
-        os.makedirs("./images")
-    query = query.split()
-    query = '+'.join(query)
-    url = "https://www.google.co.in/search?q=" + query + "&source=lnms&tbm=isch"
-    header = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-    soup = get_soup(url, header)
-
-    Images_Links = []
-    i = 0
-    print("Finding images of " + query.replace("+", " "))
-    for a in soup.find_all("div", {"class": "rg_meta"}):
-        link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
-        Images_Links.append((link, Type))
-
-    ActualImages = []
-    print("checking valid url")
-    for i in range(len(Images_Links)):
-        if Images_Links[i][0].split(".")[-1] in ["png", "jpeg", "jpg"]:
-            ActualImages.append(Images_Links[i][0])
+def scrape_google_image(query, max_num=1, name=None):
     if name is None:
         name = query
-    print("\n")
-    for i in range(num):
-        try:
-            if num > 0:
-                if name is None:
-                    urllib.request.urlretrieve(ActualImages[i], str(i) + "." + ActualImages[i].split(".")[-1])
-                    print("Images Downloaded: " + str((i + 1) / num * 100) + "%\n")
-                else:
-                    urllib.request.urlretrieve(ActualImages[i], "./images/" + name + "." + ActualImages[i].split(".")[-1])
-                    print("Images Downloaded: " + str((i + 1) / num * 100) + "%\n")
-            else:
-                break
-        except BaseException:
-            continue
-    return "./images/" + name + "." + ActualImages[0].split(".")[-1]
+    save_directory = os.path.join("images", name)
+    os.makedirs(save_directory, exist_ok=True)
+    url_query = '+'.join(query.split())
+    url = r"https://www.google.co.in/search?q=" + url_query + "&source=lnms&tbm=isch"
+    soup = get_soup(url)
+
+    print("Scraping for Images of", query)
+    n_images = 0
+    for element in itertools.takewhile(lambda _: n_images < max_num, soup.find_all("div", {"class": "rg_meta"})):
+        link = json.loads(element.text)["ou"]
+        extension = link.split(".")[-1]
+        if extension in ["png", "jpeg", "jpg"]:
+            try:
+                save_path = os.path.join(save_directory, str(n_images+1) + '.' + extension)
+                urllib.request.urlretrieve(link, save_path)
+                print("Images Downloaded:", n_images+1)
+                n_images += 1
+            except urllib.error.HTTPError:
+                pass
+    return save_directory
 
 
 if __name__ == '__main__':
-    query = input("Enter the keyword: ")
+    keyword = input("Enter the keyword: ")
     no = int(input("Enter no of images (max = 100): "))
-    address = input("Enter the directory")
-    name = input("Name of the file (for one image): ")
-    scrape_google_image(query, no, address, name)
+    file_name = input("Name of the file : ")
+    scrape_google_image(keyword, no, file_name)
