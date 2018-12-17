95e45a8dc841249b8b3e39027706b2e6fa7f45d4
Ankur Sonawane <33252312+s-ankur@users.noreply.github.com>
2018-12-17 06:59:34 +0000

    Added LXML parser (#55)

--- scrape_image_from_google_images.py
+++ scrape_image_from_google_images.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 def get_soup(url):
     header = {'User-Agent':
                   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url, headers=header)), 'html.parser')
+    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url, headers=header)), 'lxml')
 
 
 def scrape_google_image_on_demand(query, max_num=1, search_engine='www.google.co.in'):
